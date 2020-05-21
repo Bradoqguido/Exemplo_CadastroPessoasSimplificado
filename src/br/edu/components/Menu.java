58780a8378a19b4c9f330fc87dbddc2b188135af
Jeferson Eduardo Guido <bradoqguido@gmail.com>
2020-05-21 01:01:36 -0300

    Criado o form base, com a implementação da interface Cadastro e extenção da biblioteca JFrame.

--- src/br/edu/components/Menu.java
+++ src/br/edu/components/Menu.java
@@ -1,10 +1,13 @@
-package br.edu;
+package br.edu.components;
 
+import br.edu.models.Cadastro;
 import br.edu.models.Fisica;
 import br.edu.models.Juridica;
 import br.edu.models.Pessoa;
 
-public class Main {
+import javax.swing.*;
+
+public class Menu extends JFrame implements Cadastro {
     public static void main(String[] args) {
 
         // implementar interface grafica
@@ -17,5 +20,25 @@ public class Main {
         Pessoa pessoaJuridica = new Juridica();
         pessoaJuridica.setNome("pessoa2");
         System.out.println(pessoaJuridica.getNome());
+    }
+
+    @Override
+    public void cadastrar() {
+
+    }
+
+    @Override
+    public void editar() {
+
+    }
+
+    @Override
+    public void remover() {
+
+    }
+
+    @Override
+    public void listarRegistros(boolean isCPF) {
+
     }
 }
